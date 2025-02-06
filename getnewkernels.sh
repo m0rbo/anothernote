2e3aebe3444bd5d644731a2dbeec304490468296
bendily <bendily@gmail.com>
2025-02-06 11:49:27 -0600

    first files

---
+++ getnewkernels.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+#set -x
+#curl -s https://lists.suse.com/pipermail/sle-updates/2024-July/thread.html|grep -i kernel|grep -v "Live Patch"|grep -v firmware
+
+#curl -s https://lists.suse.com/pipermail/sle-updates/2024-July/thread.html|grep -i kernel|grep -v "Live Patch"|grep -v firmware|awk -F\" '{print $2}'
+#https://lists.suse.com/pipermail/sle-updates/2024-July/035872.html
+#curl -s https://lists.suse.com/pipermail/sle-updates/2024-July/035874.html
+
+#currentmonth=$(date)
+# date '+%B'
+#July
+# date '+%Y'
+#2024
+
+dyear=$(date '+%Y')
+dmonth=$(date '+%B')
+dnano=$(date +%N)
+#dmonth=May
+if [ ! -f /srv/www/htdocs/kernellist/$dyear-$dmonth.html ]
+then
+touch /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+echo "<br><a href="http://192.168.0.46/kernellist/"> Home</a><br><br><br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+cp /srv/www/htdocs/kernellist/$dyear-$dmonth.html /tmp/$dyear-$dmonth.html-$dnano
+else
+mv /srv/www/htdocs/kernellist/$dyear-$dmonth.html /tmp/$dyear-$dmonth.html-$dnano
+echo "<br><a href="http://192.168.0.46/kernellist/"> Home</a><br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+fi
+
+#rm /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+for i in $(curl -s https://lists.suse.com/pipermail/sle-updates/$dyear-$dmonth/thread.html|grep -i kernel|grep -v "Live Patch"|grep -v firmware|awk -F\" '{print $2}')
+do
+curl -s https://lists.suse.com/pipermail/sle-updates/$dyear-$dmonth/$i -o /tmp/$i
+#DEBUGOUTPUT-uncomment the echo below
+#echo "list thread ID: "$i
+#	echo $i #uncomment to check the list of threads fount
+ccount=$( awk '/SUSE Linux Enterprise Server [0-9]/ { ver=$0 } /kernel-default-[0-9]/ { print ver "\n" $0 }' < /tmp/$i |head -n2|wc -l)
+
+#DEBUGOUTPUT-uncomment the echo below
+#echo "number of threads found: "$ccount
+#awk '/SUSE Linux Enterprise Server [0-9]/ { ver=$0 } /kernel-default-[0-9]/ { print ver "\n" $0 }' < /tmp/$i |head -n2|wc -l
+
+if [ "$ccount" -eq "2" ]
+then
+#	echo $ccount #uncomment to check the count of found files.
+	echo "<a href="https://lists.suse.com/pipermail/sle-updates/$dyear-$dmonth/$i"> $dyear-$dmonth-$i</a>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	echo "<br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	echo "<b>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	awk '/SUSE Linux Enterprise Server [0-9]/ { ver=$0 } /kernel-default-[0-9]/ { print ver "<br>\n" $0 }' < /tmp/$i |head -n2 >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	echo "</b>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	echo "<br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	grep "TITLE=\"SUSE-SU" /tmp/$i  >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	echo "<br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	grep "UTC $dyear" /tmp/$i|sed 's/<I>//'|sed -e 's/<\/I>//' >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html 
+	echo "<br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	#grep "Announcement ID" /tmp/$i
+	echo "<br>" >> /srv/www/htdocs/kernellist/$dyear-$dmonth.html
+	#echo "" >> /srv/www/htdocs/kernellist.html
+fi
+
+rm /tmp/$i
+
+
+
+done
+
+if [ -f /srv/www/htdocs/kernellist/$dyear-$dmonth.html ]
+then
+	diff -q /srv/www/htdocs/kernellist/$dyear-$dmonth.html /tmp/$dyear-$dmonth.html-$dnano
+	if [ $? -ne 0 ]
+	then
+	#	cat /srv/www/htdocs/kernellist/$dyear-$dmonth.html;echo "http://192.168.0.46/kernellist/"|mail -s "New Kernels found" bendily@gmail.com
+		echo "http://192.168.0.46/kernellist/ $(cat /srv/www/htdocs/kernellist/$dyear-$dmonth.html|pandoc -f html -t plain)"|mail -s "New Kernels found" bendily@gmail.com
+	fi
+fi
+#<a href="https://btrfs.wiki.kernel.org/index.php/Using_Btrfs_with_Multiple_Devices">BTRFS Wiki - Using Btrfs with Multiple Devices</a>
+#ls -latr /srv/www/htdocs/kernellist/ |grep html|grep -v index|awk '{print $9}' > index.html
+
+ls -latr /srv/www/htdocs/kernellist/|grep html|grep -v index|awk '{print "<a href=\"http://192.168.0.46/kernellist/"$9"\">"$9"</a><br>"}' > /srv/www/htdocs/kernellist/index.html
+rm /tmp/$dyear-$dmonth.html-$dnano
---
+++ newTIDs.sh
@@ -0,0 +1,39 @@
+TEMPFILE=$(mktemp)
+TEMPFILE2=$(mktemp)
+mv /tmp/tidlist.txt $TEMPFILE
+#the sort on this curl puts "12" (december) over/on top of "01" (january) which puts all the new january TIDs out of the list.
+#curl -Ls 'https://www.suse.com/support/kb/?maxIndex=300&startIndex=1' | hxnormalize -ex | hxselect '#results' | w3m -T text/html -dump -cols 1024 | tac | tr '\n' '\0' | xargs -0 -L2 | sort -Vr | nl|head -n20 > /tmp/tidlist.txt
+
+#at this time, (Jan 17 2025), this curl is showing the january TIDs at the top of the list
+curl -Ls 'https://www.suse.com/support/kb/?maxIndex=10&startIndex=1' | hxnormalize -ex | hxselect '#results' | w3m -T text/html -dump -cols 1024 |tr '\n' '\0' | xargs -0 -L2 |nl|head -n 20 > /tmp/tidlist.txt
+
+
+#awk 'BEGIN{FS="\\(00"}{print $2}' /tmp/tidlist.txt |sed s'/)//'
+#for i in $(curl -s https://lists.suse.com/pipermail/sle-updates/$dyear-$dmonth/thread.html|grep -i kernel|grep -v "Live Patch"|grep -v firmware|awk -F\" '{print $2}')
+IFS='
+'
+for f in $(cat /tmp/tidlist.txt)
+do
+	#     1	11/13/2024 How to check if an NFS mount point supports xattr (000021619)
+	#     <a href="https://www.facebook.com/Seravo" class="social-facebook"><span>Facebook</span></a>
+#	echo $f
+	TIDNUM=$(echo $f|awk 'BEGIN{FS="\\(00"}{print $2}' |sed s'/)//')
+	#https://www.suse.com/support/kb/doc/?id=000019214
+#	echo $tidnum
+	TIDLINK="https://www.suse.com/support/kb/doc/?id=00$TIDNUM"
+#	TIDLINK="<a href="https://www.suse.com/support/kb/doc/?id=00$TIDNUM" <span>$TIDNUM</span></a>"
+
+echo $f $TIDLINK
+echo $f $TIDLINK >> $TEMPFILE2
+done
+  diff -q /tmp/tidlist.txt $TEMPFILE
+        if [ $? -ne 0 ]
+        then
+        #       cat /srv/www/htdocs/kernellist/$dyear-$dmonth.html;echo "http://192.168.0.46/kernellist/"|mail -s "New Kernels found" bendily@gmail.com
+       #         cat /tmp/tidlist.txt |mail -s "New TIDs found" bendily@gmail.com
+		cat $TEMPFILE2 |mail -s "New TIDs found" bendily@gmail.com
+        fi
+
+#	done
+rm $TEMPFILE
+rm $TEMPFILE2
